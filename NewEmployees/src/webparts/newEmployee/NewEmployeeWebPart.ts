01ce1a181ad3ab95bc0085c2cae7dfca729774ed
Tesfaye Gari <TGari@chemtrec.com>
2021-07-31 13:31:22 -0400

    final class Employee Webpart Completed with assignment 7-31

--- NewEmployees/src/webparts/newEmployee/NewEmployeeWebPart.ts
+++ NewEmployees/src/webparts/newEmployee/NewEmployeeWebPart.ts
@@ -1,6 +1,7 @@
 import { Version } from '@microsoft/sp-core-library';
 import {
   IPropertyPaneConfiguration,
+  PropertyPaneDropdown,
   PropertyPaneSlider,
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
@@ -13,11 +14,16 @@ export interface INewEmployeeWebPartProps {
   description: string;
   listName: string;
   maxItems: number;
+  viewType: string;
 }
 
 export default class NewEmployeeWebPart extends BaseClientSideWebPart<INewEmployeeWebPartProps> {
 
   public render(): void {    
+    //injecting bootstrap css 
+    let bootstrap4Url = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
+    SPComponentLoader.loadCss(bootstrap4Url);
+    //end of bootstrap injection
     const  webUrl = this.context.pageContext.web.absoluteUrl;
     console.log('Your Site Url is ', webUrl);
     const empListApiUrl = `${webUrl}/_api/web/lists/getbytitle('${this.properties.listName}')/items?$top=${this.properties.maxItems}`;
@@ -26,41 +32,78 @@ export default class NewEmployeeWebPart extends BaseClientSideWebPart<INewEmploy
       
       console.log('Reading SharePoint Data Successfully ', response);
       let items = response.value;
-      console.log(`We have ${items.length} items responded. `, items)
-    
-    }, error => console.error('Oops error occured', error));
-    
-   let bootstrap4Url = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
-   SPComponentLoader.loadCss(bootstrap4Url);
-
-    this.domElement.innerHTML = `
-    <h2>${this.properties.description} </h2>
-    ${this.renderRowHtml(null)}   
+      console.log(`We have ${items.length} items responded. `, items);
+      //render the Dom element
+      this.domElement.innerHTML = `
+                <h2>${this.properties.description} </h2>
+                ${this.renderRowHtml(items)}   
                         `;
+    }, error => console.error('Oops error occured', error));
+         
   }
 
   private getSPData(apiUrl: string) {
      return this.context.spHttpClient
       .get(apiUrl, SPHttpClient.configurations.v1)
       .then(response => {
-        return response.json()
+        return response.json();
       });
   }
 
   private renderRowHtml(rows){
+    let rowsHtml = '';
+    for(let item of rows){
+      rowsHtml += this.renderColHtml(item);
+    }
+
     return `<div class="row">
-              <div class="col">
-                <div class="card">
-                  <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image"
-                    style="width:100%">
-                  <div class="card-body">
-                    <h4 class="card-title">John Doe</h4>
-                    <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
-                    <a href="#" class="btn btn-primary">Read more ... </a>
-                  </div>
-                </div>
-              </div>     
+              ${this.properties.viewType=="Grid"? rowsHtml :this.renderTableHtml()}
             </div> `;
+  }
+
+  private renderColHtml(item){
+    let web = this.context.pageContext.web.absoluteUrl;
+    return `<div class="col-sm-4 mb-2">
+              <div class="card">
+                <img class="card-img-top" src="${item.Photo?item.Photo:'https://randomuser.me/api/portraits/men/50.jpg'}" alt="Card image"
+                  style="width:100%">
+                <div class="card-body">
+                  <h4 style="font-size: 1em;" class="card-title">${item.Title}</h4>
+                  <p class="card-text">Phone: ${item.Phone?item.Phone:'No Phone'}</p>
+                  <p class="card-text">${item.Bio?item.Bio.substring(0, 60):'No Bio'}...</p>
+                  <a href="${web}/Lists/Employees/DispForm.aspx?ID=${item.Id}" target="_blank" class="btn btn-primary">Read more ... </a>
+                </div>
+              </div>
+            </div>`;
+  }
+
+  private renderTableHtml(){
+    return `<table class="table">
+              <thead class="thead-dark">
+                <tr>
+                  <th>Full Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>John Doe</td>                 
+                  <td>john@example.com</td>
+                  <td>444-543-5544</td>
+                </tr>
+                <tr>
+                  <td>Mary Moe</td>                  
+                  <td>mary@example.com</td>
+                  <td>444-543-5544</td>
+                </tr>
+                <tr>
+                  <td>July Dooley</td>                  
+                  <td>july@example.com</td>
+                  <td>445-369-2587</td>
+                </tr>
+              </tbody>
+            </table>`;
   }
   protected get disableReactivePropertyChanges(): boolean {
     return true;
@@ -95,6 +138,12 @@ export default class NewEmployeeWebPart extends BaseClientSideWebPart<INewEmploy
                   value: 5,
                   showValue: true,
                   step: 1
+                }),
+                PropertyPaneDropdown('viewType', {
+                  label: 'Select Your View',
+                  options: [
+                    {key: 'Table', text: 'Table'},
+                    {key: 'Grid', text: 'Grid'}]
                 })
               ]
             }
